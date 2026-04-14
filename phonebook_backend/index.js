e328e06528d20afe9a5beaa7f9f9688803085937
Kael <269341204+kurosaki-dev@users.noreply.github.com>
2026-04-14 12:09:36 +0800

    3.7: Phonebook backend step 7 & 3.8*: Phonebook backend step 8

--- phonebook_backend/index.js
+++ phonebook_backend/index.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
 
 app.use(express.json());
+
+morgan.token("type", (req, res) => {
+  return JSON.stringify(req.body);
+});
+
+app.use(morgan(":method :url :status - :response-time ms :type"));
 
 let data = [
   {
